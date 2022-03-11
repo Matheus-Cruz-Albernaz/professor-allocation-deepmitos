956915f877a0e30cf8dd97ef4cb58752163f497b
Rodrigo Souza <rodrigosein@gmail.com>
2022-03-11 19:53:35 -0300

    Test Repository

--- src/test/java/com/project/professor/allocation/grupo2/repository/AllocationRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/grupo2/repository/AllocationRepositoryTest.java
@@ -1,5 +1,114 @@
-package com.project.professor.allocation.grupo2.repository;
+package com.project.professor.allocation.repository;
 
+import com.project.professor.allocation.entity.Allocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
 public class AllocationRepositoryTest {
 
+	SimpleDateFormat sdf = new SimpleDateFormat("HH:mmZ");
+
+	@Autowired
+	AllocationRepository allocationRepository;
+
+	@Test
+	public void findAll() {
+		// Act
+		List<Allocation> allocations = allocationRepository.findAll();
+
+		// Print
+		System.out.println(allocations);
+	}
+
+	@Test
+	public void findById() {
+		// Arrange
+
+		// Act
+		Optional<Allocation> optional = allocationRepository.findById(2L);
+
+		// Print
+		Allocation allocation = optional.orElse(null);
+		System.out.println(allocation);
+	}
+
+	@Test
+	public void findByProfessorId() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void findByCourseId() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void save_create() throws ParseException {
+		// Arrange
+		Allocation allocation = new Allocation();
+		allocation.setDay(DayOfWeek.MONDAY);
+		allocation.setEnd(sdf.parse("12:00-0300"));
+		allocation.setStart(sdf.parse("10:00-0300"));
+		allocation.setProfessorId(1L);
+		allocation.setCourseId(2L);
+		// Act
+		Allocation alloc = allocationRepository.save(allocation);
+		// Print
+		System.out.println(alloc);
+	}
+
+	@Test
+	public void save_update() throws ParseException {
+		// Arrange
+		Allocation allocation = new Allocation();
+		allocation.setId(3L);
+		allocation.setDay(DayOfWeek.MONDAY);
+		allocation.setEnd(sdf.parse("12:00-0300"));
+		allocation.setStart(sdf.parse("10:00-0300"));
+		allocation.setProfessorId(1L);
+		allocation.setCourseId(2L);
+		// Act
+		Allocation alloc = allocationRepository.save(allocation);
+		// Print
+		System.out.println(alloc);
+	}
+
+	@Test
+	public void deleteById() {
+		// Arrange
+
+		// Act
+		allocationRepository.deleteById(11L);
+	}
+
+	@Test
+	public void deleteAll() {
+		// Act
+		allocationRepository.deleteAllInBatch();
+	}
 }
--- src/test/java/com/project/professor/allocation/grupo2/repository/CourseRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/grupo2/repository/CourseRepositoryTest.java
@@ -1,5 +1,99 @@
-package com.project.professor.allocation.grupo2.repository;
+package com.project.professor.allocation.repository;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professor.allocation.entity.Course;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
 
 public class CourseRepositoryTest {
 
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Test
+	public void findAll() {
+		// Act
+		List<Course> course = courseRepository.findAll();
+
+		// Print
+		System.out.println(course);
+	}
+
+	@Test
+	public void findById() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void findByProfessorId() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void findByCourseId() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void save_create() throws ParseException {
+		// Arrange
+		Course course = new Course();
+		course.setName("'Design");
+		// Act
+		Course curso = courseRepository.save(course);
+		// Print
+		System.out.println(curso);
+	}
+
+	@Test
+	public void save_update() throws ParseException {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void deleteById() {
+		// Arrange
+
+		// Act
+
+	}
+
+	@Test
+	public void deleteAll() {
+		// Act
+
+	}
 }
