f47985a2d34d2b94bde9f07cb21d54851dc191f2
Matheus Cruz Albernaz <matheusalbernaz2003@gmail.com>
2022-03-11 18:03:51 -0300

    Add Config Classes

--- src/main/java/com/project/professor/allocation/grupo2/config/CorsConfig.java
+++ src/main/java/com/project/professor/allocation/grupo2/config/CorsConfig.java
@@ -1,5 +1,25 @@
 package com.project.professor.allocation.grupo2.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		
+		return new WebMvcConfigurer() {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowedOrigins("*");
+			}
+		};
+	}
 
 }
