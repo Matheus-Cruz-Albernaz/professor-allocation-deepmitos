8b3826bdc0e17beb99b8d77688a207dc61dbb559
Gabriel Viana <gabrielfs.viana2@gmail.com>
2022-03-11 20:01:12 -0300

    add professor entity

--- src/main/java/com/project/professor/allocation/grupo2/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/grupo2/entity/Professor.java
@@ -1,5 +1,75 @@
 package com.project.professor.allocation.grupo2.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "name", nullable = false)
+	private String name;
+	@Column(length = 14, unique = true, nullable = false)
+	private String cpf;
+	
+	@Column(name="department_id", nullable = false)
+	private Long departmentId;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "department_id", updatable = false, insertable = false, nullable = false)
+	private Department depart;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public Department getDepart() {
+		return depart;
+	}
+
+	public void setDepart(Department depart) {
+		this.depart = depart;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [id=" + id + ", name=" + name + ", cpf=" + cpf + ", departmentId=" + departmentId
+				+ ", depart=" + depart + "]";
+	}
 
 }
