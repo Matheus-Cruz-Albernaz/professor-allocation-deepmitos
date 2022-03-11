17ff9f7f2ed5236f398e938cf70b1ed90c1d3fe1
Gabriel Viana <gabrielfs.viana2@gmail.com>
2022-03-11 20:00:29 -0300

    add course entity

--- src/main/java/com/project/professor/allocation/grupo2/entity/Course.java
+++ src/main/java/com/project/professor/allocation/grupo2/entity/Course.java
@@ -1,5 +1,40 @@
 package com.project.professor.allocation.grupo2.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false, unique = true)
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
 
 }
