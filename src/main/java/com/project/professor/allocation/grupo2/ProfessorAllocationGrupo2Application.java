74b552f1f0438b225753a8493d9092871f52fe84
Matheus Cruz Albernaz <matheusalbernaz2003@gmail.com>
2022-03-10 22:06:05 -0300

    Arquivos Spring

---
+++ src/main/java/com/project/professor/allocation/grupo2/ProfessorAllocationGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationGrupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/grupo2/ProfessorAllocationGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
