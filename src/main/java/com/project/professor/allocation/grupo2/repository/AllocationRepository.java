3aaff83e3dd6e13ee66cb5614cc56916fccf4645
Rodrigo Souza <rodrigosein@gmail.com>
2022-03-11 19:49:42 -0300

    Add AllocationRepository

--- src/main/java/com/project/professor/allocation/grupo2/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/grupo2/repository/AllocationRepository.java
@@ -1,5 +1,17 @@
-package com.project.professor.allocation.grupo2.repository;
+package com.project.professor.allocation.repository;
 
-public interface AllocationRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.professor.allocation.entity.Allocation;
+
+@Repository
+public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+	
+	List<Allocation> findByProfessorId(Long professorId);
+	
+	List<Allocation> findByCourseId(Long courseId);
+		
 }
